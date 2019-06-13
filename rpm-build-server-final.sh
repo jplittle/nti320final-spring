866b0fe0580909ec3ba4e82eafafbe84dcb1fbc5
mnichols <mnichols-github@outlook.com>
2019-06-13 09:20:39 -0700

    mod

--- rpm-build-server-final.sh
+++ rpm-build-server-final.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-# install the correct programs for building & dependencies needed:
-yum -y install rpm-build make gcc git
+yum -y install rpm-build make gcc git                                         # install rpm tools, compiling tools and source tools
 
-# to wget things from the web 
-yum -y install wget
-
-# Create the directory structure we'll use for our build process:
-mkdir -p -v /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-
+mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}                      # create the rpmbuild dirrectory structure
+ 
+                                                                              
 cd ~/
- # Set the rpmbuild path in an .rpmmacros file
-echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros                         # Set the rpmbuild path in an .rpmmacros file
+
 cd ~/rpmbuild/SOURCES
 
-#git clone
-#cp NTI-320/rpm-info/hello_world_from_source/helloworld-0.1.tar.gz .
-#.sh
-#.spec
-#mv .spec ../SPECS
-#cd ..
+git clone https://github.com/nic-instruction/NTI-320.git
 
-# to_build
-#rpmbuild -v -bb --clean SPECS/hello.spec
+cd NTI-320/
+cp NTI-320/rpm-info/hello_world_from_source/helloworld-0.1.tar.gz .
+cp NTI-320/rpm-info/hello_world_from_source/helloworld.sh .
+cp NTI-320/rpm-info/hello_world_from_source/hello.spec .
+mv hello.spec ../SPECS
 
+cd ..
 
-#yum -y install RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm
+rpmbuild -v -bb --clean SPECS/hello.spec
+
+##did it work?
+#ls -l RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm
+
+yum -y install RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm
+
+##did it work?
+#ls -l /
