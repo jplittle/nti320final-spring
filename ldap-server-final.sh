902f5183aa6bd521940a15d91cdb9c1f0987ec3a
mnichols <mnichols-github@outlook.com>
2019-06-13 11:19:16 -0700

    mod

--- ldap-server-final.sh
+++ ldap-server-final.sh
@@ -9,7 +9,7 @@ yum install -y git
 #change directory----------------------------
 cd /tmp
 #clone repository-------------------------------
-git clone https://github.com/mnichols-github/NTI310final.git
+git clone https://github.com/mnichols-github/nti320final-spring.git
 #install ldap server and ldap client------------------------------
 yum -y install openldap-servers
 yum -y install openldap-clients
@@ -37,7 +37,7 @@ unalias cp
 #create backup config.php
 cp /etc/phpldapadmin/config.php /etc/phpldapadmin/config.php.orig
 #copy config.php to phpldapadmin.conf-------
-cp /tmp/NTI310final/config.php /etc/phpldapadmin/config.php
+cp /tmp/nti320final-spring/config.php /etc/phpldapadmin/config.php
 #change ownership of config.php-------------
 chown ldap:apache /etc/phpldapadmin/config.php
 #change to tmp directory
@@ -55,12 +55,12 @@ chmod 0600 /root/ldap_admin_pass
 echo -e "dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 replace: olcSuffix
-olcSuffix: dc=nti310,dc=local
+olcSuffix: dc=nti320,dc=local
 \n
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 replace: olcRootDN
-olcRootDN: cn=ldapadm,dc=nti310,dc=local
+olcRootDN: cn=ldapadm,dc=nti320,dc=local
 \n
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
@@ -71,21 +71,21 @@ ldapmodify -Y EXTERNAL  -H ldapi:/// -f db.ldif
 echo 'dn: olcDatabase={1}monitor,cn=config
 changetype: modify
 replace: olcAccess
-olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external, cn=auth" read by dn.base="cn=ldapadm,dc=nti310,dc=local" read by * none' > monitor.ldif
+olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external, cn=auth" read by dn.base="cn=ldapadm,dc=nti320,dc=local" read by * none' > monitor.ldif
 ldapmodify -Y EXTERNAL  -H ldapi:/// -f monitor.ldif
 # Generate certs-------------------------------------------
-openssl req -new -x509 -nodes -out /etc/openldap/certs/nti310ldapcert.pem -keyout /etc/openldap/certs/nti310ldapkey.pem -days 365 -subj "/C=US/ST=WA/L=Seattle/O=SCC/OU=IT/CN=nti310.local"
+openssl req -new -x509 -nodes -out /etc/openldap/certs/nti320ldapcert.pem -keyout /etc/openldap/certs/nti320ldapkey.pem -days 365 -subj "/C=US/ST=WA/L=Seattle/O=SCC/OU=IT/CN=nti320.local"
 chown -R ldap. /etc/openldap/certs/nti*.pem
 # Use Certs in LDAP----------------------------------------
 echo -e "dn: cn=config
 changetype: modify
 replace: olcTLSCertificateKeyFile
-olcTLSCertificateKeyFile: /etc/openldap/certs/nti310ldapkey.pem
+olcTLSCertificateKeyFile: /etc/openldap/certs/nti320ldapkey.pem
 \n
 dn: cn=config
 changetype: modify
 replace: olcTLSCertificateFile
-olcTLSCertificateFile: /etc/openldap/certs/nti310ldapcert.pem" > certs.ldif
+olcTLSCertificateFile: /etc/openldap/certs/nti320ldapcert.pem" > certs.ldif
 ldapmodify -Y EXTERNAL -H ldapi:/// -f certs.ldif
 #adding schema templates
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
@@ -93,42 +93,42 @@ ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 # Create base group and people structure----------------
 #create domain ldap manager account---------------
-echo -e "dn: dc=nti310,dc=local
-dc: nti310
+echo -e "dn: dc=nti320,dc=local
+dc: nti320
 objectClass: top
 objectClass: domain
 \n
-dn: cn=ldapadm ,dc=nti310,dc=local
+dn: cn=ldapadm ,dc=nti320,dc=local
 objectClass: organizationalRole
 cn: ldapadm
 description: LDAP Manager
 \n
 #create main Group Group------------------
-dn: ou=Group,dc=nti310,dc=local
+dn: ou=Group,dc=nti320,dc=local
 objectClass: organizationalUnit
 ou: Group
 \n
 #create child Group-A---------------------------
-dn: cn=Group-A,ou=Group,dc=nti310,dc=local
+dn: cn=Group-A,ou=Group,dc=nti320,dc=local
 cn: Group-A
 gidnumber: 500
 objectclass: posixGroup
 objectclass: top
 \n
 #create child Group-B-------------------------
-dn: cn=Group-B,ou=Group,dc=nti310,dc=local
+dn: cn=Group-B,ou=Group,dc=nti320,dc=local
 cn: Group-B
 gidnumber: 501
 objectclass: posixGroup
 objectclass: top
 \n
 #create main Group People---------------------
-dn: ou=People,dc=nti310,dc=local
+dn: ou=People,dc=nti320,dc=local
 objectClass: organizationalUnit
 ou: People
 \n
 #create child People <users>------------------
-dn: cn=name-0 lname-0,ou=People,dc=nti310,dc=local
+dn: cn=name-0 lname-0,ou=People,dc=nti320,dc=local
 cn: name-0 lname-0
 gidnumber: 500
 givenname: name-0
@@ -142,7 +142,7 @@ uid: name-0
 uidnumber: 1000
 userpassword: {SHA}8qEvGH67cIC9darJFgIU5rHkn30=
 \n
-dn: cn=name-1 lname-1,ou=People,dc=nti310,dc=local
+dn: cn=name-1 lname-1,ou=People,dc=nti320,dc=local
 cn: name-1 lname-1
 gidnumber: 500
 givenname: name-1
@@ -156,7 +156,7 @@ uid: name-1
 uidnumber: 1001
 userpassword: {SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=
 \n
-dn: cn=name-2 lname-2,ou=People,dc=nti310,dc=local
+dn: cn=name-2 lname-2,ou=People,dc=nti320,dc=local
 cn: name-2 lname-2
 gidnumber: 500
 givenname: name-2
@@ -170,7 +170,7 @@ uid: name-2
 uidnumber: 1002
 userpassword: {SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=
 \n
-dn: cn=name-3 lname-3,ou=People,dc=nti310,dc=local
+dn: cn=name-3 lname-3,ou=People,dc=nti320,dc=local
 cn: name-3 lname-3
 gidnumber: 500
 givenname: name-3
@@ -184,7 +184,7 @@ uid: name-3
 uidnumber: 1003
 userpassword: {SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=
 \n
-dn: cn=name-4 lname-4,ou=People,dc=nti310,dc=local
+dn: cn=name-4 lname-4,ou=People,dc=nti320,dc=local
 cn: name-4 lname-4
 gidnumber: 500
 givenname: name-4
@@ -198,7 +198,7 @@ uid: name-4
 uidnumber: 1004
 userpassword: {SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=
 \n
-dn: cn=name-5 lname-5,ou=People,dc=nti310,dc=local
+dn: cn=name-5 lname-5,ou=People,dc=nti320,dc=local
 cn: name-5 lname-5
 gidnumber: 500
 givenname: name-5
@@ -214,7 +214,7 @@ userpassword: {SHA}W6ph5Mm5Pz8GgiULbPgzG37mj9g=" > base.ldif
 #temporary change - sets SElinux to permissive mode
 setenforce 0
 #set ldap simple auth, prompt for simple auth, bind DN to ldap dir for SASL auth
-ldapadd -x -W -D "cn=ldapadm,dc=nti310,dc=local" -f base.ldif -y /root/ldap_admin_pass
+ldapadd -x -W -D "cn=ldapadm,dc=nti320,dc=local" -f base.ldif -y /root/ldap_admin_pass
 #restart httpd
 systemctl restart httpd
 #install, config  rsyslog client
