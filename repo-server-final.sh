3c1f775f7066e5d94c7c3498655b4efaac9930d5
mnichols <mnichols-github@outlook.com>
2019-06-13 09:15:59 -0700

    mod

--- repo-server-final.sh
+++ repo-server-final.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-yum -y install createrepo
-yum -y install httpd
-yum -y install wget
+yum -y install createrepo                                                          	# Install createrepo
 
-mkdir -v -p /repos/centos/7/extras/x86_64/Packages/
+mkdir -p /repos/centos/7/extras/x86_64/Packages/                                  	# Make your repo dir
 
-# cp helloworld-0.1-1.el7.x86_64.rpm /repos/centos/7/extras/x86_64/Packages/
+cp helloworld-0.1-1.el7.x86_64.rpm /repos/centos/7/extras/x86_64/Packages   		# Replace 'helloworld*' with your package
 
-createrepo --update /repos/centos/7/extras/x86_64/Packages/
+createrepo --update /repos/centos/7/extras/x86_64/Packages/                        	# Update after every change
 
-setenforce 0
+yum -y install httpd                                                              	# Now install apache so you can serve your repo over the web
+
+setenforce 0										# disable selinux
+
 systemctl enable httpd
+
 systemctl start httpd
 
-ln -s  /repos/centos /var/www/html/centos
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
+ln -s  /repos/centos /var/www/html/centos                                         	# Link your repos in
 
-sed -i '144i     Options All' /etc/httpd/conf/httpd.conf
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak                      	# Make a copy of our origonal httpd.conf
+
+sed -i '144i     Options All' /etc/httpd/conf/httpd.conf                          	# Configure apache
 sed -i '145i    # Disable directory index so that it will index our repos' /etc/httpd/conf/httpd.conf
 sed -i '146i     DirectoryIndex disabled' /etc/httpd/conf/httpd.conf
-sed -i 's/^/#/' /etc/httpd/conf.d/welcome.conf
+
+sed -i 's/^/#/' /etc/httpd/conf.d/welcome.conf                                    	# Disables the defualt welcome page in the recommended way
 
 chown -R apache:apache /repos/
+
 systemctl restart httpd
