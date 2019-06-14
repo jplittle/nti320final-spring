abfe2de065d2984bb43cc2ba80b5e3402dc542fc
mnichols <mnichols-github@outlook.com>
2019-06-13 17:44:45 -0700

    mod

--- scp_to_nagios.sh
+++ scp_to_nagios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bash /home/mnicho18/nti320final-spring/generate_config.sh $1 $2
-gcloud compute scp --zone us-west1-b $1.cfg g42dfyt@nti320-final-nagios-server:/etc/nagios/servers
+gcloud compute scp --zone us-west1-b $1.cfg mnich018@nagios-server-final:/etc/nagios/servers
 # Note: 
 #Nagios server code near bottom: I had to add user nicolebade to group nagios on nagios server using usermod -a -G nagios mnicho18
 # I also had to chmod 777 /etc/nagios/servers
