0e13af3e6ddd12fc640113918cd527a02e8e3664
mnichols <mnichols-github@outlook.com>
2019-06-13 18:26:41 -0700

    mod

--- gcloud-final.sh
+++ gcloud-final.sh
@@ -10,60 +10,53 @@ gcloud compute instances create rsyslog-server-final \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-west1-b \
-#--private-network-ip=10.138.0.204 \
+--private-network-ip=10.138.0.204 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/rsyslog-server-final.sh
-sleep 30s
 
 #repo
 gcloud compute instances create repo-server-final \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-west1-b \
-#--private-network-ip=10.138.0.200 \
+--private-network-ip=10.138.0.200 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/repo-server-final.sh
-sleep 30s
 
 #rpm
-gcloud compute instances rpm-build-server-final \
+gcloud compute instances create rpm-build-server-final \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-west1-b \
-#--private-network-ip=10.138.0.201 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/rpm-build-server-final.sh
-sleep 30s
 
 #nagios
-gcloud compute instances nagios-server-final \
+gcloud compute instances create nagios-server-final \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-west1-b \
-#--private-network-ip=10.138.0.202 \
+--private-network-ip=10.138.0.202 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/nagios-server-final.sh
-sleep 30s
 
 #cacti
-gcloud compute instances cacti-server-final \
+gcloud compute instances create cacti-server-final \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-west1-b \
-#--private-network-ip=10.138.0.203 \
 --tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/cacti-server-final.sh
-sleep 30s
 
 #ldap
 gcloud compute instances create ldap-server-final \
@@ -74,7 +67,6 @@ gcloud compute instances create ldap-server-final \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/ldap-server-final.sh
-sleep 30s
 
 #nfs
 gcloud compute instances create nfs-server-final \
@@ -85,7 +77,6 @@ gcloud compute instances create nfs-server-final \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/nfs-server-final.sh
-sleep 30s
 
 #postgres front
 gcloud compute instances create postgres-server-front-final \
@@ -96,7 +87,6 @@ gcloud compute instances create postgres-server-front-final \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/postgres-server-front-final.sh
-sleep 30s
      
 #postgress back
 gcloud compute instances create postgres-server-back-final \
@@ -107,8 +97,7 @@ gcloud compute instances create postgres-server-back-final \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/postgres-server-back-final.sh
-sleep 30s
-      
+
 #ldap-nfs client - 1 
 gcloud compute instances create ldap-nfs-client-01 \
 --image-family ubuntu-1804-lts \
@@ -118,7 +107,6 @@ gcloud compute instances create ldap-nfs-client-01 \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti320final-spring/ldap-nfs-client.sh    
-sleep 30s
 
 #ldap-nfs client- 2
 gcloud compute instances create ldap-nfs-client-02 \
